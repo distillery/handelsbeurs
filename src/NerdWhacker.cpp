17f3040715fe81275d0e5eef9977c30fcf6800da
Wim Roose <systeembeheer@vooruit.be>
2015-12-15 09:08:30 +0100

    Fixed typo

--- src/NerdWhacker.cpp
+++ src/NerdWhacker.cpp
@@ -21,8 +21,8 @@ void NerdWhacker::setup() {
     countCycles = 0;
     bSendSerialMessage = true;
     serial.enumerateDevices();
-    serial.setup(0, 115200);
-    
+    //serial.setup(0, 115200);
+    serial.setup("/dev/ttyACM0", 115200);
     // GAME
     game = new Game();
 }
@@ -31,8 +31,8 @@ void NerdWhacker::setup() {
 
 void NerdWhacker::update() {
     
-//    if (serial.isInitialized()==1) {
-//        cout << "serial.setup = ok" << endl;
+  //  if (serial.isInitialized()==1) {
+   //     cout << "serial.setup = ok" << endl;
 
         // ARDUINO WRITE
         if (bSendSerialMessage) {
@@ -81,6 +81,7 @@ void NerdWhacker::update() {
             // vervangen door score_string!?
             unsigned char buf[NUM_BYTES_OUT] = {s1, s2, s3, l, t1, t2, t3, r1, r2, r3};
             serial.writeBytes(&buf[0], NUM_BYTES_OUT);
+            cout << "Serial buffer: " << s1 << s2 << s3 << l << t1 << t2 << t3 << endl;
             
             bSendSerialMessage = false;
         }
@@ -89,11 +90,11 @@ void NerdWhacker::update() {
             bSendSerialMessage = true;
             countCycles = 0;
         }
-
+        
         // ARDUINO READ
         unsigned char bytesReturned[NUM_BYTES_IN];
         memset(bytesReturned, 0, NUM_BYTES_IN);
-        while (serial.readBytes(bytesReturned, NUM_BYTES_IN) > 0) {};
+        while (serial.readBytes(bytesReturned, NUM_BYTES_IN) > 0) {
 
         string plx = ofToString(bytesReturned);
 
@@ -118,6 +119,8 @@ void NerdWhacker::update() {
                 case 'v': game->hitPlexi(11); break; // v = plexi 12
             }
         }
+        
+    }
 //    }
 //    else {
 //        cout << "(re)try serial.setup" << endl;
@@ -143,22 +146,22 @@ void NerdWhacker::draw() {
 
 
 void NerdWhacker::keyReleased(int key) {
-    
-    if (key==OF_KEY_CONTROL) {
+    cout << key << endl;
+    if (key==10) { // PRESS ENTER FOR CONFIG MODE
         game->setup = !game->setup;
     }
     
     if (game->setup) {
         switch (key) {
-            case 'a': game->selectPlexi(0); break; // a = plexi 1
-            case 'z': game->selectPlexi(1); break; // z = plexi 2
+            case 'q': game->selectPlexi(0); break; // a = plexi 1
+            case 'w': game->selectPlexi(1); break; // z = plexi 2
             case 'e': game->selectPlexi(2); break; // e = plexi 3
             case 'r': game->selectPlexi(3); break; // r = plexi 4
-            case 'q': game->selectPlexi(4); break; // q = plexi 5
+            case 'a': game->selectPlexi(4); break; // q = plexi 5
             case 's': game->selectPlexi(5); break; // s = plexi 6
             case 'd': game->selectPlexi(6); break; // d = plexi 7
             case 'f': game->selectPlexi(7); break; // f = plexi 8
-            case 'w': game->selectPlexi(8); break; // w = plexi 9
+            case 'z': game->selectPlexi(8); break; // w = plexi 9
             case 'x': game->selectPlexi(9); break; // x = plexi 10
             case 'c': game->selectPlexi(10); break; // c = plexi 11
             case 'v': game->selectPlexi(11); break; // v = plexi 12
@@ -167,21 +170,21 @@ void NerdWhacker::keyReleased(int key) {
             case OF_KEY_DOWN: game->movePlexi("y", 1); break;
             case OF_KEY_LEFT: game->movePlexi("x", -1); break;
             case OF_KEY_RIGHT: game->movePlexi("x", 1); break;
-            case OF_KEY_COMMAND: game->resizePlexi(1); break;
-            case OF_KEY_ALT: game->resizePlexi(-1); break;
+            case 111: game->resizePlexi(1); break; // O
+            case 108: game->resizePlexi(-1); break;  // L
         }
     }
     else {
         switch (key) {
-            case 'a': game->hitPlexi(0); break; // a = plexi 1
-            case 'z': game->hitPlexi(1); break; // z = plexi 2
+            case 'q': game->hitPlexi(0); break; // a = plexi 1
+            case 'w': game->hitPlexi(1); break; // z = plexi 2
             case 'e': game->hitPlexi(2); break; // e = plexi 3
             case 'r': game->hitPlexi(3); break; // r = plexi 4
-            case 'q': game->hitPlexi(4); break; // q = plexi 5
+            case 'a': game->hitPlexi(4); break; // q = plexi 5
             case 's': game->hitPlexi(5); break; // s = plexi 6
             case 'd': game->hitPlexi(6); break; // d = plexi 7
             case 'f': game->hitPlexi(7); break; // f = plexi 8
-            case 'w': game->hitPlexi(8); break; // w = plexi 9
+            case 'z': game->hitPlexi(8); break; // w = plexi 9
             case 'x': game->hitPlexi(9); break; // x = plexi 10
             case 'c': game->hitPlexi(10); break; // c = plexi 11
             case 'v': game->hitPlexi(11); break; // v = plexi 12
